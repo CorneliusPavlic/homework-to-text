ad6308e610fdcbcfefdd47f604a2f8f78593b27a
Cornelius Pavlic <cornelius.pavlic@gmail.com>
2024-05-14 14:12:46 -0400

    Very basic page

---
+++ homeworkapp/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+       <input type="file" id="myFile" name="filename" capture="user"></input>
+    </div>
+  );
+}
+
+export default App;
