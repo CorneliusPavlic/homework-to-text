8ee15eb83791f92b6ae19b3296cc7d7675406208
Cornelius Pavlic <cornelius.pavlic@gmail.com>
2024-06-01 00:45:22 -0400

    Added new model

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,9 @@ import google.generativeai as genai
 from PIL import Image  # Import Image directly from PIL
 from pdf2image import convert_from_path
 import io  # For handling in-memory files
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
@@ -16,6 +19,7 @@ CORS(app, resources={r"/api/sendFiles": {"origins": "http://localhost:3000"}})
 @app.route('/api/sendFiles', methods=['POST'])
 @cross_origin(origins="http://localhost:3000")
 def upload_file():
+    model = load_model('./mnist_digit_recognition_model.h5')
     result_string = ''
     files = request.files.getlist('file')  # Get multiple files
 
@@ -35,7 +39,7 @@ def upload_file():
         if file.find('.pdf',0) != -1:
             continue
         else:
-            description = getGeminiResponse('images/' + file)  # Pass the file path
+            description = getModelResponse('images/' + file, model)  # Pass the file path
             result_string += description
 
     # Delete all images in the 'images' directory after processing
@@ -45,28 +49,15 @@ def upload_file():
     return result_string  # Return the processed description
 
 
-load_dotenv()
-GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
-genai.configure(api_key=GEMINI_API_KEY)  # Configure Google Generative AI
 
-
-def getGeminiResponse(file_path):
-    """Analyzes a math image using Gemini Pro Vision and returns LaTeX expressions."""
-    print("making Gemini Call")
-    model = genai.GenerativeModel('gemini-pro-vision')
-    with Image.open(file_path) as img:  # Open the image using Image.open
-        # Convert image to bytes for Gemini Pro Vision
-        img_bytes = io.BytesIO()
-        img.save(img_bytes, format='PNG')
-        response = model.generate_content(
-            ['If this image contains math problems Analyze it for each math problem and list each one in LaTeX', img],
-            stream=True
-        )
-        print("done with gemini call")
-        response.resolve()
-        print(response.text)  # Print the Gemini response
-        return response.text  # Return the content
-
+def getModelResponse(image_path, model):
+    thresh_image = preprocess_image(image_path)
+    digit_contours = find_digit_contours(thresh_image)
+    predictions = classify_digits(model, thresh_image, digit_contours)
+    output_string = ''
+    for predicted_digit, (x, y, w, h) in predictions:
+        output_string += str(predicted_digit)
+    return output_string
 
 
 def save_pdf_pages_as_png(pdf_path, output_dir="images"):
@@ -81,6 +72,38 @@ def save_pdf_pages_as_png(pdf_path, output_dir="images"):
         page_number += 1
 
 
+def classify_digits(model, image, contours):
+    digit_predictions = []
+    for contour in contours:
+        # Compute the bounding box of the contour
+        x, y, w, h = cv2.boundingRect(contour)
+
+        # Crop and resize the image around the contour
+        digit = image[y:y+h, x:x+w]
+        digit = cv2.resize(digit, (28, 28))
+        digit = np.expand_dims(digit, axis=-1)
+        digit = np.expand_dims(digit, axis=0).astype('float32') / 255
+
+        # prediction
+        prediction = model.predict(digit)
+        predicted_digit = np.argmax(prediction, axis=1)[0]  # Get the index of max predicted value
+        digit_predictions.append((predicted_digit, (x, y, w, h)))
+
+    return digit_predictions
+
+
+def find_digit_contours(preprocessed_image):
+    contours, _ = cv2.findContours(preprocessed_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+
+def preprocess_image(image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    _, thresh = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY_INV)
+    return thresh
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
