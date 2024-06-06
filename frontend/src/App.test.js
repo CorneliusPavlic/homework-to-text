5952118257fa69d8ac1051989eb538f9c11c4752
Cornelius Treadway Pavlic <43487455+CorneliusPavlic@users.noreply.github.com>
2024-06-06 13:06:15 -0400

    Delete frontend directory

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Scanner from './Scanner';
-
-test('renders learn react link', () => {
-  render(<Scanner />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
